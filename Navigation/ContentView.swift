4baef96453c192d2ea4a97d116a6d890115fee25
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-27 11:50:17 -0700

    finished product

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                Text("This is the root view 🌳")
+                    .fontWeight(.bold)
+                
+                    .toolbar {
+                        ToolbarItemGroup(placement: .status) {
+                            NavigationLink(destination: SecondView()) {
+                                Text("About")
+                            }
+                        }
+                    }
+            }
+            
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            
         }
-        .padding()
     }
+        
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("You made it to the second view!")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
